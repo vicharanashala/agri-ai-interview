a2dfef085572e3c1e11d228f5d029bc219e73491
Thanha Mariyam <thanhathanha665@gmail.com>
2026-08-02 21:06:29 +0530

    fix overall color theme and header

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.watchOptions = {
+        poll: 1000,
+        aggregateTimeout: 300,
+      };
+    }
+    return config;
+  },
   async rewrites() {
     return {
       beforeFiles: [
